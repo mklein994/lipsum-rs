f69a5e4ac4c00af754d8f9e4de4040f2507ccf07
Matthew Klein <mklein994@gmail.com>
2018-06-17 16:54:34 -0500

    Add method to print paragraphs
    Needs a lot of work; including the rand crate.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,24 @@
 #[macro_use]
 extern crate clap;
+extern crate lipsum;
 
 pub mod app;
+
+pub fn get_paragraphs(count: usize) -> String {
+    let mut p: String = String::new();
+    for _ in 0..count {
+        p.push_str(&format!("{}\n", lipsum::lipsum(1)));
+    }
+    p
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn get_10_paragraphs() {
+        let actual = get_paragraphs(10);
+        assert_eq!(10, actual.lines().count());
+    }
+}
