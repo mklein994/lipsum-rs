2a24cd40b5804692d9316325c0522950963ae32c
Matthew Klein <mklein994@gmail.com>
2018-12-06 20:25:32 -0600

    Upgrade to rust 2018 edition and move run() to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,34 @@
 #[macro_use]
 extern crate clap;
-extern crate lipsum;
-extern crate rand;
 
 use rand::distributions::{Distribution, Uniform};
 
 /// Define the command line arguments.
 pub mod app;
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let m = app::build_cli().get_matches();
+
+    if m.is_present("title") {
+        println!("{}", lipsum::lipsum_title());
+
+        // If there are more orguments, or if `-w` was passed, add a newline after the title.
+        if m.args.len() > 2 || m.occurrences_of("words") == 1 {
+            println!();
+        }
+    }
+
+    if m.is_present("paragraphs") {
+        let count = value_t!(m.value_of("paragraphs"), usize).unwrap_or(3);
+        paragraphs(count);
+    }
+
+    if m.occurrences_of("words") == 1 || m.args.len() == 1 {
+        let count = value_t!(m.value_of("words"), usize)?;
+        words(count);
+    }
+    Ok(())
+}
 
 /// Print a title.
 pub fn title() {
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,5 @@
-extern crate lipsum_rs;
-#[macro_use]
-extern crate clap;
-extern crate lipsum;
-
-use lipsum_rs::app;
-
-fn run() -> Result<(), Box<std::error::Error>> {
-    let m = app::build_cli().get_matches();
-
-    if m.is_present("title") {
-        println!("{}", lipsum::lipsum_title());
-
-        // If there are more orguments, or if `-w` was passed, add a newline after the title.
-        if m.args.len() > 2 || m.occurrences_of("words") == 1 {
-            println!();
-        }
-    }
-
-    if m.is_present("paragraphs") {
-        let count = value_t!(m.value_of("paragraphs"), usize).unwrap_or(3);
-        lipsum_rs::paragraphs(count);
-    }
-
-    if m.occurrences_of("words") == 1 || m.args.len() == 1 {
-        let count = value_t!(m.value_of("words"), usize)?;
-        lipsum_rs::words(count);
-    }
-    Ok(())
-}
-
 fn main() {
-    if let Err(e) = run() {
+    if let Err(e) = lipsum_rs::run() {
         eprintln!("{}", e);
         std::process::exit(1);
     }
